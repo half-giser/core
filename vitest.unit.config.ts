844f939b3f2cce7a6a86bec0c2c7988cfbe0201c
Kenny ZHOU <half-giser@users.noreply.github.com>
2025-05-06 01:36:37 +0000

    docs(process): update unit test files

--- vitest.unit.config.ts
+++ vitest.unit.config.ts
@@ -4,6 +4,7 @@ import config from './vitest.config'
 export default mergeConfig(config, {
   test: {
     name: 'unit',
+    include: ['packages/vue/__tests__/index.spec.ts'],
     exclude: [...configDefaults.exclude, '**/e2e/**'],
   },
 })
